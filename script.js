d0d427482831017f8957e2947c343be167465b42
Vasanthi49 <vasanthinarala01@gmail.com>
2025-04-11 17:13:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
-      e.preventDefault(); // Prevent default jump
-      
-      const targetId = this.getAttribute('href').substring(1); // Get target ID
+      e.preventDefault(); 
+      const targetId = this.getAttribute('href').substring(1); 
       const targetElement = document.getElementById(targetId);
-
       if (targetElement) {
-          targetElement.scrollIntoView({ behavior: 'smooth' }); // Smooth scroll
+          targetElement.scrollIntoView({ behavior: 'smooth' }); 
       }
   });
 });
